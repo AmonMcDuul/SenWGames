e9de000adc9a5957799e00ccbd6099b22064cb11
AmonMcDuul <joranomic@hotmail.com>
2023-06-08 21:10:34 +0200

    SignalR WIP project settings

--- SenWGames/SenWGames.Core/Domain/Entities/Group.cs
+++ SenWGames/SenWGames.Core/Domain/Entities/Group.cs
@@ -3,12 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SenWGames.Core.Domain.Entities
 {
     public class Group : BaseEntity
     {
+        public string GroupId { get; private set; }
         public string GroupName { get; private set; }
         public GameLobby GameLobby { get; private set; }
         public ICollection<GameLobby> PlayedGames { get; private set; }
@@ -17,6 +19,7 @@ namespace SenWGames.Core.Domain.Entities
 
         public Group(string groupName, GameLobby gameLobby, ICollection<GameLobby> playedGames, ICollection<Player> players, Player groupLeader)
         {
+            GroupId = Guid.NewGuid().ToString();
             GroupName = groupName;
             GameLobby = gameLobby;
             PlayedGames = playedGames;
--- SenWGames/SenWGames.Web/Program.cs
+++ SenWGames/SenWGames.Web/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using SenWGames.Infrastructure;
+using SenWGames.Web.Hubs;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,21 @@ builder.Services.AddTransient<DbContext, SenwDbContext>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// SignalR
+builder.Services.AddSignalR(options =>
+{
+    // Docs https://learn.microsoft.com/en-us/aspnet/core/signalr/configuration?view=aspnetcore-7.0&tabs=dotnet
+    options.ClientTimeoutInterval = TimeSpan.FromSeconds(66); // should be double KeepAliveInterval
+    options.KeepAliveInterval = TimeSpan.FromSeconds(33);
+})
+    .AddJsonProtocol(options =>
+    {
+        options.PayloadSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    }
+);
+
+builder.Services.AddSingleton<ISenWStateManager, SenWStateManager>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +37,14 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    // must be called before UseMvc
+    app.UseCors(builder =>
+    {
+        // this should be configured on the host. See App service > CORS. Values set here must only be used for localhost.
+        builder.AllowAnyMethod().AllowAnyHeader().WithOrigins(
+            "http://localhost:4200", "http://localhost:4201")
+        .AllowCredentials();
+    });
 }
 
 app.UseHttpsRedirection();
